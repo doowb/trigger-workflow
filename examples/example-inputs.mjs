d5fceca39897a0dff3151b7a2fe1128688ecea95
Brian Woodward <brian.woodward@gmail.com>
2024-08-09 11:54:40 -0400

    add example with inputs

--- examples/example-inputs.mjs
+++ examples/example-inputs.mjs
@@ -1,19 +1,23 @@
 import 'dotenv/config';
-import { triggerWorkflow, waitForCompletion } from './dist/index.mjs';
+import { triggerWorkflow, waitForCompletion } from '../dist/index.mjs';
 
 async function main() {
   const token = process.env.GITHUB_TOKEN || 'ghp_YourGithubTokenHere';
   const owner = 'doowb';
   const repo = 'trigger-workflow';
-  const workflow_id = 'example-workflow.yml';
+  const workflow_id = 'example-inputs.yml';
   const ref = 'main';
+  const inputs = {
+    message: 'Hello from the message input!'
+  };
 
   const run_id = await triggerWorkflow({
     owner,
     repo,
     workflow_id,
     ref,
-    token
+    token,
+    inputs
   });
 
   await waitForCompletion({
